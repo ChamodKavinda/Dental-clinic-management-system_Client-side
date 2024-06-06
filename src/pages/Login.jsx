ff75da5e81dbf2eabb8862cfccfc106b7e989f6d
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-06 21:42:39 +0530

    implement login component functions

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,59 @@ import { ToastContainer, toast } from "react-toastify";
 
 const Login = () => {
 
+    const navigate = useNavigate();
+    const [inputValue, setInputValue] = useState({
+        email: "",
+        password: "",
+    });
+    const { email, password } = inputValue;
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value,
+        });
+    };
+
+    const handleError = (err) =>
+        toast.error(err, {
+            position: "bottom-left",
+        });
+    const handleSuccess = (msg) =>
+        toast.success(msg, {
+            position: "bottom-left",
+        });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "http://localhost:4000/login",
+                {
+                    ...inputValue,
+                },
+                { withCredentials: true }
+            );
+            console.log(data);
+            const { success, message } = data;
+            if (success) {
+                handleSuccess(message);
+                setTimeout(() => {
+                    navigate("/home");
+                }, 1000);
+            } else {
+                handleError(message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        setInputValue({
+            ...inputValue,
+            email: "",
+            password: "",
+        });
+    };
+
     return(
         <div className="form_container">
             <h2>Login Account</h2>
