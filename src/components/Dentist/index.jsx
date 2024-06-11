1996568c8cbfd4dd07fc0026a6f40d6a8dac5c5e
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-11 11:51:41 +0530

    get all dentist details calling API

--- src/components/Dentist/index.jsx
+++ src/components/Dentist/index.jsx
@@ -1,7 +1,7 @@
 import Header from "../global/Header";
 import Sidebar from "../global/Sidebar";
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import {useNavigate} from 'react-router-dom';
 import { Edit, Delete } from '@mui/icons-material';
 import {
     Container,
@@ -18,46 +18,26 @@ import {
     Button,
     IconButton
 } from '@mui/material';
+import axios, {Axios} from "axios";
 
 function Dentist() {
-    const dentist = [
-        {
-            id: 'P001',
-            name: 'M.Peter',
-            age: 25,
-            phone: '0768765456',
-            sex: 'Male',
-            address: 'Colombo',
-            description: 'Regular check-up'
-        },
-        {
-            id: 'P002',
-            name: 'M.Peter',
-            age: 25,
-            phone: '0768765456',
-            sex: 'Male',
-            address: 'Colombo',
-            description: 'Dental cleaning'
-        },
-        {
-            id: 'P003',
-            name: 'M.Peter',
-            age: 25,
-            phone: '0768765456',
-            sex: 'Male',
-            address: 'Colombo',
-            description: 'Cavity filling'
-        },
-        {
-            id: 'P004',
-            name: 'M.Peter',
-            age: 25,
-            phone: '0768765456',
-            sex: 'Male',
-            address: 'Colombo',
-            description: 'Tooth extraction'
-        }
-    ];
+
+    const navigate = useNavigate();
+    const [dentist,setDentist]=useState([]);
+
+    useEffect(()=>{
+        getDentist();
+    },[])
+
+    const getDentist=()=>{
+        axios.get('http://localhost:3000/dentist/get')
+            .then(response=>{
+                setDentist(response.data || []);
+            }).catch(error=>{
+                console.error('Axios error',error);
+        })
+    }
+
 
     return (
         <>
@@ -81,32 +61,32 @@ function Dentist() {
                                         <Paper sx={{ p: 2 }}>
                                             <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
                                                 <Typography variant="h6"></Typography>
-                                                <Button variant="contained" color="primary">Add Dentist</Button>
+                                                <Button variant="contained" color="primary" onClick={()=>navigate('/addDentist') }>Add Dentist</Button>
                                             </Box>
                                             <TableContainer component={Paper}>
                                                 <Table>
                                                     <TableHead>
                                                         <TableRow>
-                                                            <TableCell>Dentist ID</TableCell>
-                                                            <TableCell>Dentist Name</TableCell>
-                                                            <TableCell>Age</TableCell>
-                                                            <TableCell>Phone Number</TableCell>
-                                                            <TableCell>Sex</TableCell>
-                                                            <TableCell>Address</TableCell>
-                                                            <TableCell>Description</TableCell>
-                                                            <TableCell>Actions</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Dentist ID</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Dentist Name</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Age</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Phone Number</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Sex</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Address</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Description</TableCell>
+                                                            <TableCell sx={{fontWeight:'revert',fontSize:'revert'}}>Actions</TableCell>
                                                         </TableRow>
                                                     </TableHead>
                                                     <TableBody>
-                                                        {dentist.map((dentist, index) => (
-                                                            <TableRow key={index}>
-                                                                <TableCell>{dentist.id}</TableCell>
-                                                                <TableCell>{dentist.name}</TableCell>
-                                                                <TableCell>{dentist.age}</TableCell>
-                                                                <TableCell>{dentist.phone}</TableCell>
-                                                                <TableCell>{dentist.sex}</TableCell>
-                                                                <TableCell>{dentist.address}</TableCell>
-                                                                <TableCell>{dentist.description}</TableCell>
+                                                        {dentist.length > 0 ? dentist.map(row=> (
+                                                            <TableRow key={row.id}>
+                                                                <TableCell component='th' scope="row">{row.id}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.name}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.age}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.number}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.sex}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.address}</TableCell>
+                                                                <TableCell component='th' scope="row">{row.description}</TableCell>
                                                                 <TableCell>
                                                                     <IconButton color="primary">
                                                                         <Edit />
@@ -116,7 +96,12 @@ function Dentist() {
                                                                     </IconButton>
                                                                 </TableCell>
                                                             </TableRow>
-                                                        ))}
+                                                        )):(
+                                                            <TableRow >
+                                                                <TableCell>No Data</TableCell>
+                                                            </TableRow>
+                                                        )
+                                                        }
                                                     </TableBody>
                                                 </Table>
                                             </TableContainer>
