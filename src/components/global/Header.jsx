80e46eee5fc9c138e99a216d70bb9b326d5a5260
chamod kavinda <kavindachamod100@gmail.com>
2024-06-17 01:12:14 +0530

    modify header

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,130 +1,86 @@
-import '../Styles.css';
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState } from 'react';
+import {useNavigate,Link} from "react-router-dom";
+import { AppBar, Toolbar, Typography, Avatar, Box, Menu, MenuItem, ListItemIcon } from '@mui/material';
+import { Settings, Logout } from '@mui/icons-material';
 import teeth from '../../assets/teeth.png';
-import { MdMenuOpen } from "react-icons/md";
-import Button from "@mui/material/Button";
-import SearchBox from "../SearchBox";
-import { MdOutlineLightMode } from "react-icons/md";
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-
-
-const Header = () => {
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
+const Header = ({logout}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+    const navigate = useNavigate();
+
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
+
     const handleClose = () => {
         setAnchorEl(null);
     };
 
-    const navigate = useNavigate();
-    const handleLoginClick = path => {
+    const handleLoginClick = (path) => {
         navigate(path);
     };
 
     return (
-        <header className="d-flex align-items-center">
-            <div className="container-fluid w-100">
-                <div className="row d-flex align-items-center w-100">
-                    <div className="col-sm-2 part1">
-                        <Link to={'/dashboard'} className="d-flex align-items-center logo">
-                            <img src={teeth} />
-                            <span className="ml-2">Dental Pro Care </span>
+        <AppBar position="static" sx={{ bgcolor:"#8AA899" }}>
+            <Toolbar>
+                <Box component="img" src={teeth} sx={{ width: 70, height: 40, marginRight: 2 , bgcolor:'#CAE7DF', cursor:'pointer'}} onClick={()=>navigate('/dashboard')} />
+                <Typography variant="h5" sx={{ flexGrow: 1 , color:'white', fontWeight:'bold',cursor:'pointer'}} onClick={()=>navigate('/dashboard')}>
+                    Dental Pro Care
+                </Typography>
 
-                        </Link>
-                    </div>
-                    <div className="col-sm-3 d-flex align-items-center part2 pl-4">
-                        <Button className="rounded-circle mr-3"><MdMenuOpen /></Button>
-                        <SearchBox/>
-                    </div>
-
-                    <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
-                        <Button className="rounded-circle mr-3" ><MdOutlineLightMode /></Button>
-
-
-
-                        <div className="myAccWrapper">
-                            <Button className="myAcc d-flex align-items-center" onClick={handleClick}>
-                                <div className="userImg">
-                                <span className="rounded-circle">
-                                    <img src="https://thumbs.dreamstime.com/z/unknown-male-avatar-profile-image-businessman-vector-unknown-male-avatar-profile-image-businessman-vector-profile-179373829.jpg?w=768" alt=""/>
-                                </span>
-                                </div>
-                                <div className="userInfo">
-                                    <h4>Kamal</h4>
-                                    <p className="mb-0">kamal100@gmail.com</p>
-                                </div>
-                            </Button>
-
-                            <Menu
-                                anchorEl={anchorEl}
-                                id="account-menu"
-                                open={open}
-                                onClose={handleClose}
-                                onClick={handleClose}
-                                PaperProps={{
-                                    elevation: 0,
-                                    sx: {
-                                        overflow: 'visible',
-                                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                        mt: 1.5,
-                                        '& .MuiAvatar-root': {
-                                            width: 32,
-                                            height: 32,
-                                            ml: -0.5,
-                                            mr: 1,
-                                        },
-                                        '&::before': {
-                                            content: '""',
-                                            display: 'block',
-                                            position: 'absolute',
-                                            top: 0,
-                                            right: 14,
-                                            width: 10,
-                                            height: 10,
-                                            bgcolor: 'background.paper',
-                                            transform: 'translateY(-50%) rotate(45deg)',
-                                            zIndex: 0,
-                                        },
-                                    },
-                                }}
-                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                            >
-                                <MenuItem onClick={handleClose}>
-                                    <Avatar /> My account
-                                </MenuItem>
-                                <MenuItem onClick={handleClose}>
-                                    <ListItemIcon>
-                                        <Settings fontSize="small" />
-                                    </ListItemIcon>
-                                    Settings
-                                </MenuItem>
-                                <MenuItem onClick={() => handleLoginClick('/')}>
-                                    <ListItemIcon>
-                                        <Logout fontSize="small" />
-                                    </ListItemIcon>
-                                    Logout
-                                </MenuItem>
-                            </Menu>
-
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-        </header>
+                <Box display="flex" alignItems="center" onClick={handleClick} sx={{ cursor: 'pointer' }}>
+                    <Avatar alt="Kamal" />
+                    <Box ml={2} sx={{color:'white'}}>
+                        <Typography>Kamal</Typography>
+                        <Typography variant="body2">kamalsilva100@gmail.com</Typography>
+                    </Box>
+                </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&::before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={() => {logout()}}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </Toolbar>
+        </AppBar>
     );
-}
+};
 
 export default Header;
