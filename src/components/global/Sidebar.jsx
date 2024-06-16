673f3f59a969d6c033a6284ec0583a5c6fdac9ab
chamod kavinda <kavindachamod100@gmail.com>
2024-06-17 01:12:28 +0530

    modify sidebar

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -1,66 +1,52 @@
-import Button from '@mui/material/Button';
-import { LuLayoutDashboard } from "react-icons/lu";
-import { FaUserDoctor } from "react-icons/fa6";
-import { GrUserWorker } from "react-icons/gr";
-import { MdCalendarMonth } from "react-icons/md";
-import { FaWheelchair } from "react-icons/fa";
-import { FaAngleRight } from "react-icons/fa";
-import {useNavigate} from "react-router-dom";
+import React from 'react';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import { Dashboard, Event, Person, MedicalServices, Group } from '@mui/icons-material';
+import { Link, useLocation } from 'react-router-dom';
 
+const Sidebar = () => {
+    const location = useLocation();
 
-const  Sidebar = ()=>{
+    const getListItemStyle = (path) => {
+        return location.pathname === path ? { backgroundColor: '#DDEDF4', color: 'black' } : {};
+    };
 
-    const navigate = useNavigate();
-
-
-    return(
-        <>
-            <div className="sidebar">
-                <ul>
-                    <li>
-                            <Button className="w-100" onClick={()=> navigate('/dashboard')}>
-                                <span className="icon"><LuLayoutDashboard/></span>
-                                Dashboard
-                            </Button>
-
-                    </li>
-
-                    <li>
-                        <Button className="w-100" onClick={()=> navigate('/appointment')}>
-                            <span className="icon"><MdCalendarMonth/></span>
-                            Appointment
-                            {/*<span className="arrow"><FaAngleRight/></span>*/}
-                        </Button>
-                    </li>
-
-                    <li>
-                        <Button className="w-100" onClick={()=> navigate('/patient')}>
-                            <span className="icon"><FaWheelchair/></span>
-                            Patient
-                            {/*<span className="arrow"><FaAngleRight/></span>*/}
-                        </Button>
-                    </li>
-
-                    <li>
-                        <Button className="w-100" onClick={()=> navigate('/dentist')}>
-                            <span className="icon"><FaUserDoctor/></span>
-                            Dentist
-                            {/*<span className="arrow"><FaAngleRight/></span>*/}
-                        </Button>
-                    </li>
-
-                    <li>
-                        <Button className="w-100" onClick={()=> navigate('/staff')}>
-                            <span className="icon"><GrUserWorker/></span>
-                            Staff
-                            {/*<span className="arrow"><FaAngleRight/></span>*/}
-                        </Button>
-                    </li>
-                </ul>
-            </div>
-
-        </>
+    return (
+        <Box sx={{ width: 240, height: '100vh', backgroundColor: '#95D2B3', color: 'white' }}>
+            <List>
+                <ListItem button component={Link} to="/dashboard" sx={getListItemStyle('/dashboard')}>
+                    <ListItemIcon>
+                        <Dashboard sx={{ color: location.pathname === '/dashboard' ? 'white' : 'black' }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItem>
+                <ListItem button component={Link} to="/appointment" sx={getListItemStyle('/appointment')}>
+                    <ListItemIcon>
+                        <Event sx={{ color: location.pathname === '/appointment' ? 'white' : 'black' }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Appointment" />
+                </ListItem>
+                <ListItem button component={Link} to="/patient" sx={getListItemStyle('/patient')}>
+                    <ListItemIcon>
+                        <Person sx={{ color: location.pathname === '/patient' ? 'white' : 'black' }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Patient" />
+                </ListItem>
+                <ListItem button component={Link} to="/dentist" sx={getListItemStyle('/dentist')}>
+                    <ListItemIcon>
+                        <MedicalServices sx={{ color: location.pathname === '/dentist' ? 'white' : 'black' }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Dentist" />
+                </ListItem>
+                <ListItem button component={Link} to="/staff" sx={getListItemStyle('/staff')}>
+                    <ListItemIcon>
+                        <Group sx={{ color: location.pathname === '/staff' ? 'white' : 'black' }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Employee" />
+                </ListItem>
+            </List>
+            <Divider />
+        </Box>
     );
-}
+};
 
-export default Sidebar
+export default Sidebar;
