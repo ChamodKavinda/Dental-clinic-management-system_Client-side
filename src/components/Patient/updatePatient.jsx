6b6483c679afc04ba816543a36621ea60932bfe4
chamod kavinda <kavindachamod100@gmail.com>
2024-06-16 09:37:19 +0530

    implement Update Patient Component

---
+++ src/components/Patient/updatePatient.jsx
@@ -0,0 +1,232 @@
+
+import React, {useEffect,useState} from "react";
+import axios from 'axios';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import {
+    Container,
+    Box,
+    Paper,
+    Typography,
+    Grid,
+    TextField,
+    MenuItem,
+    Button
+} from '@mui/material';
+
+
+function UpdatePatient({payload,onClose,getPatient}) {
+
+    const [id,setId] = useState('');
+    const [name,setName] = useState('');
+    const [age,setAge] = useState('');
+    const [number,setNumber] = useState('');
+    const [sex,setSex] = useState('');
+    const [address,setAddress] = useState('');
+    const [description,setDescription] = useState('');
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+    const [touched, setTouched] = useState({
+        id: false,
+        name: false,
+        age: false,
+        number: false,
+        sex: false,
+        address: false,
+        description: false
+    });
+
+    const handleReset = () => {
+        setName('');
+        setAge('');
+        setNumber('');
+        setSex('');
+        setAddress('');
+        setDescription('');
+        setTouched({
+            name: false,
+            age: false,
+            number: false,
+            sex: false,
+            address: false,
+            description: false
+        });
+    };
+
+    const handleSuccess = (msg) =>
+        toast.success(msg, {
+            position:'top-right'
+        });
+
+    useEffect(() => {
+        if (payload) {
+            setId(payload.id || '');
+            setName(payload.name || '');
+            setAge(payload.age || '');
+            setNumber(payload.number || '');
+            setSex(payload.sex || '');
+            setAddress(payload.address || '');
+            setDescription(payload.description || '');
+        }
+    }, [payload]);
+
+
+    useEffect(() => {
+        if (id && name && age && number && sex && address) {
+            setIsSubmitDisabled(false);
+        } else {
+            setIsSubmitDisabled(true);
+        }
+    }, [id, name, age, number, sex, address]);
+
+
+    const handleSubmit = (e) => {
+        handleReset();
+        e.preventDefault();
+        const payload = {
+            id: id,
+            name: name,
+            age: age,
+            number: number,
+            sex: sex,
+            address: address,
+            description: description
+        };
+
+        axios.put('http://localhost:3000/patient/update', payload)
+            .then(response => {
+                console.log(response);
+            }).catch(error => {
+            console.error('Axios error :', error);
+        });
+        onClose();
+        handleSuccess('Updated Successfully');
+        getPatient();
+    };
+
+    const handleBlur = (field) => {
+        setTouched({ ...touched, [field]: true });
+    };
+
+    return (
+        <>
+            <div className="main d-flex">
+                <Box sx={{ display: 'flex', flexGrow: 1, bgcolor: '#F7F7F7' }}>
+                    <Box component="main" sx={{ flexGrow: 1, p: 0}}>
+                        <Container maxWidth="lg">
+                            <Box display="flex" justifyContent="space-between" alignItems="center" mb={3}>
+                            </Box>
+                            <ToastContainer />
+                            <Paper sx={{ p: 0 }}>
+                                <form onSubmit={handleSubmit}>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                fullWidth
+                                                label="Patient ID"
+                                                name="id"
+                                                variant="outlined"
+                                                value={id}
+                                                onChange={e =>{setId(e.target.value)}}
+                                                onBlur={() => handleBlur('id')}
+                                                disabled
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                fullWidth
+                                                label="Patient Name"
+                                                name="name"
+                                                variant="outlined"
+                                                value={name}
+                                                onChange={e =>{setName(e.target.value)}}
+                                                onBlur={() => handleBlur('name')}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                fullWidth
+                                                label="Age"
+                                                name="age"
+                                                variant="outlined"
+                                                type="number"
+                                                value={age}
+                                                onChange={e =>{setAge(e.target.value)}}
+                                                onBlur={() => handleBlur('age')}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                fullWidth
+                                                label="Phone Number"
+                                                name="phone"
+                                                variant="outlined"
+                                                type="number"
+                                                value={number}
+                                                onChange={e =>{setNumber(e.target.value)}}
+                                                onBlur={() => handleBlur('number')}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                select
+                                                fullWidth
+                                                label="Sex"
+                                                name="sex"
+                                                variant="outlined"
+                                                value={sex}
+                                                onChange={e =>{setSex(e.target.value)}}
+                                                onBlur={() => handleBlur('sex')}
+                                            >
+                                                <MenuItem value="Male">Male</MenuItem>
+                                                <MenuItem value="Female">Female</MenuItem>
+                                            </TextField>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <TextField
+                                                fullWidth
+                                                label="Address"
+                                                name="address"
+                                                variant="outlined"
+                                                value={address}
+                                                onChange={e =>{setAddress(e.target.value)}}
+                                                onBlur={() => handleBlur('address')}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                fullWidth
+                                                label="Description"
+                                                name="description"
+                                                variant="outlined"
+                                                multiline
+                                                rows={4}
+                                                value={description}
+                                                onChange={e =>{setDescription(e.target.value)}}
+                                                onBlur={() => handleBlur('description')}
+
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Box display="flex" justifyContent="flex-end">
+                                                <Button type="reset" variant="contained" color="error" sx={{marginRight:'10px'}}
+                                                        onClick={handleReset}>
+                                                    Reset
+                                                </Button>
+                                                <Button type="submit" variant="contained" color="primary"  disabled={isSubmitDisabled}>
+                                                    Update
+                                                </Button>
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Paper>
+                        </Container>
+                    </Box>
+                </Box>
+            </div>
+        </>
+    );
+}
+
+export default UpdatePatient;
